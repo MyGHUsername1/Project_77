6254711e96403b522545704241cbffd70c24f964
MyGHUsername1 <78768719+MyGHUsername1@users.noreply.github.com>
2021-07-13 22:52:37 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var firstName = "Harry ";
+console.log(firstName);
+var lastName = "Doe";
+console.log(lastName);
+var first_lastName = firstName.concat(lastName);
+console.log(first_lastName);
+var num1 = 19
+console.log(num1);
+var num2 = 14
+console.log(num2);
+var add = num1 + num2
+console.log(add);
+var MATH = num1*num2
+console.log(MATH)
